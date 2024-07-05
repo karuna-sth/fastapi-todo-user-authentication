857bc214430e5f8291525f23e13ba77f79806117
karuna-sth <karunashrestha824@gmail.com>
2024-07-05 23:25:40 +0545

    Added docker

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-import APIComponents.models as models
+from APIComponents import models
 from APIComponents.database import engine
 from APIComponents.routers import todo, users, authentication
 
@@ -16,4 +16,4 @@ app.include_router(authentication.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
